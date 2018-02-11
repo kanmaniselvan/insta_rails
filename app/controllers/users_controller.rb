f23322d5b63b631e3ac0716f376318561d87d790
Kanmani <kanmaniselvan.m@in.reportbee.com>
2018-02-11 15:58:25 +0530

    If user already has signed_in, then redirect to feeds path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   skip_before_action :require_login, only: :home
 
   def home
-
+    if user_signed_in?
+      redirect_to feeds_path
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe UsersController, type: :controller do
 
     expect(response.body).to match(/Login with Instagram/)
   end
+
+  it 'redirects to feeds page if user has already signed in' do
+    session[:user_id] = create(:user)
+
+    get(:home)
+
+    expect(response.header['Location']).to match(/feeds/)
+  end
 end
